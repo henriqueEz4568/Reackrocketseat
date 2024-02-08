a0a4f08a9e49dfc2f42e4e97169d3fffb969f323
henriqueEz4568 <rick.scioli@gmail.com>
2024-02-07 21:35:43 -0300

    começando a modularizar

--- rs/src/App.jsx
+++ rs/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 
 
 import { Header } from './components/Header'
+import { Post } from './post'
 
 
 function App() {
  
 
-  return (
+  return (<div>
  <Header />
+  <Post 
+  author="henrique"
+  content="O Cristiano Ronaldo, ele ta carregando Solskjaer, ele ta carregando Pogba, ele ta carregando Rashford, ele ta carregando a família Gleizer, o Cristiano Ronaldo ta carregando o Old Trafford, ele ta carregando tudo sozinho. ELE TA BOTANDO ESSA CHAMPIONS NO BOLSO! Essa Champions é de Cristiano Ronaldo, ele pegou a fase de grupos da Champions e colocou no bolso. EU RESOLVO. SE NINGUÉM FAZ NADA, SE NÃO TEM TRABALHO TÁTICO, SE NÃO TEM COLETIVO, SE NÃO TEM FUTEBOL, EU RESOLVO. EU SOU O SENHOR CHAMPIONS UEFA, CRISTIANO RONALDO LEAGUE. É isso e acabou!?"
+  />
+  
+  
+  
+ </div>  
   )
 }
 
--- rs/src/components/header.tsx
+++ rs/src/components/header.tsx
@@ -5,7 +5,7 @@ import styles from './Header.module.css';
 export function Header() {
   return (
     <header className={styles.header}>
-      ignite
+      ignite fuck off
     </header>
   );
 }
